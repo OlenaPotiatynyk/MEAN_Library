8a7abce25583719d0d93991f59cad1541bfd0126
Olena Potiatynyk <olena.potiatynyk@gmail.com>
2023-05-06 17:24:12 -0700

    Add home page design

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -5,6 +5,28 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  list = [
+    {
+      file: 'Some file name 01',
+      description: 'Some loooooong description',
+      owner: 'Some Good Guy'
+    },
+    {
+      file: 'Some file name 02',
+      description: 'Some loooooong description',
+      owner: 'Some Good Guy'
+    },
+    {
+      file: 'Some file name 03',
+      description: 'Some loooooong description',
+      owner: 'Some Good Guy'
+    },
+    {
+      file: 'Some file name 04',
+      description: 'Some loooooong description',
+      owner: 'Some Good Guy'
+    }
+  ]
 }
